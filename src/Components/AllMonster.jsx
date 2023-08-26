f375efe1fc6023e9f10ae317015b614906d24148
sabbirz <sabbir.kuasha@gmail.com>
2023-08-26 11:54:21 +0600

    Model added

--- src/Components/AllMonster.jsx
+++ src/Components/AllMonster.jsx
@@ -13,11 +13,11 @@ export default function Experience() {
       {/* <color attach="background" args={["#ececec"]} /> */}
       <Environment preset="city" />
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
-      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      <PerspectiveCamera makeDefault fov={50} position={[0, 0, 7]} />
       <mesh castShadow receiveShadow>
         {/* <boxGeometry args={[1, 1, 1]} /> */}
 
-        <sphereGeometry args={[7, 128, 128]} />
+        <sphereGeometry args={[10, 128, 128]} />
         <meshStandardMaterial map={map} side={BackSide} />
       </mesh>
       <mesh castShadow receiveShadow>
