65d6dd3c8c310f693b820c7f024473383ada8be6
sabbirz <sabbir.kuasha@gmail.com>
2023-08-26 23:56:14 +0600

    animation activated to the model
    animation activated to the model

--- src/Components/AllMonster.jsx
+++ src/Components/AllMonster.jsx
@@ -12,12 +12,12 @@ export default function Experience() {
   return (
     <>
       {/* <color attach="background" args={["#ececec"]} /> */}
-      <Environment preset="city" />
+      <Environment preset="sunset" />
+      <ambientLight intensity={2.5} />
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
       <PerspectiveCamera makeDefault fov={50} position={[0, 0, 8]} />
       <mesh castShadow receiveShadow>
         {/* <boxGeometry args={[1, 1, 1]} /> */}
-
         <sphereGeometry args={[15, 128, 128]} />
         <meshStandardMaterial map={map} side={BackSide} />
       </mesh>
